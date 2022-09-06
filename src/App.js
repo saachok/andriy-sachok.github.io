cfe87ad5ec3935ac93ed2cc6d4d66dd9915b79ea
Andriy <andriy.sachok@gmail.com>
2022-09-06 17:13:34 +0300

    added img container

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import classes from './App.module.css';
+
+const DUMMY_GALLERY = [
+  {
+    title: 'Happy Pepe',
+    path: './img/pepe img/happy-pepe.png',
+    id: 'happy-pepe',
+  },
+];
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className={classes.app}>
+      <div className={classes['gallery-control']}>
+        <button>Prev img</button>
+        <img
+          src={DUMMY_GALLERY[0].path}
+          alt="Image of Pepe"
+          className={classes['current-img']}
+        />
+        <button>Next img</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
