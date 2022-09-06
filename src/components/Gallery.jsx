3427170c8d82ac22651a35fe0a33999487bc86fc
Andriy <andriy.sachok@gmail.com>
2022-09-06 18:28:16 +0300

    added scroll logic to gallry

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import GalleryItem from './GalleryItem';
 import classes from './Gallery.module.css';
 
 const Gallery = (props) => {
+  const [index, setIndex] = useState(+0);
+
+  const gallery = props.gallery;
+  // console.log('gallery length:', gallery.length);
+
+  const showPrevImg = () => {
+    setIndex((prevValue) => {
+      if (prevValue === 0) {
+        return gallery.length - 1;
+      }
+      return prevValue - 1;
+    });
+  };
+
+  const showNextImg = () => {
+    setIndex((prevValue) => {
+      if (prevValue === gallery.length - 1) {
+        return 0;
+      }
+      return prevValue + 1;
+    });
+  };
+
   return (
     <div className={classes.gallery}>
-      <button>Prev img</button>
-      <GalleryItem gallery={props.gallery} />
-      <button>Next img</button>
+      <button onClick={showPrevImg}>Prev img</button>
+      <GalleryItem path={gallery[index].path} />
+      <button onClick={showNextImg}>Next img</button>
     </div>
   );
 };
--- src/components/GalleryItem.jsx
+++ src/components/GalleryItem.jsx
@@ -3,9 +3,7 @@ import React from 'react';
 import classes from './GalleryItem.module.css';
 
 const GalleryItem = (props) => {
-  const gallery = props.gallery;
-
-  return <img src={gallery[0].path} className={classes.image} />;
+  return <img src={props.path} className={classes.image} />;
 };
 
 export default GalleryItem;
